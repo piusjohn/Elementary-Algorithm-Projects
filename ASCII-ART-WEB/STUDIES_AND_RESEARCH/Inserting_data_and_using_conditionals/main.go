bd44b6b1cd4c3d068991551a04701d693a789aeb
pijohn <epictalesk@gmail.com>
2026-05-26 01:35:10 +0100

    added some files

--- ASCII-ART-WEB/STUDIES_AND_RESEARCH/Inserting_data_and_using_conditionals/main.go
+++ ASCII-ART-WEB/STUDIES_AND_RESEARCH/Inserting_data_and_using_conditionals/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"html/template"
+)
+
+var tpl *template.Template
+
+type User struct{
+	Name string
+	language string
+	member bool
+}
+var U User
 func main() {
+	u = User{Name: "peter", language: "English", member: false}
+	tpl, _ = tpl.ParseGlob("templates/*.html")
 	
 }
