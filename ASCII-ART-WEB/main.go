b496184c59af771ef7da17b35c163502c14984c9
pijohn <epictalesk@gmail.com>
2026-05-30 10:07:27 +0100

    added some changes

--- ASCII-ART-WEB/main.go
+++ ASCII-ART-WEB/main.go
@@ -6,25 +6,37 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	type pagedata struct {
+type PageData struct {
 		Title  string
 		Result string
-	}
-	if r.URL.Path != "/" {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	data := pagedata{
-		Title:  "Ascii Art generator",
-		Result: "Ascii Art result will appear here",
-	}
-	tmpl.Execute(w, data)
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.Error(w, "Not Found", http.StatusNotFound)
+        return
+    }
+    data := PageData{Title: "ASCII Art Generator"}
+
+    if r.Method == "POST" {
+		banner := r.FormValue("banner")
+		if banner ==""{
+			http.Error(w, "Wrong Request: Invalid banner selected", http.StatusBadRequest)
+			return
+		}
+		text := r.FormValue("text")
+		if text == ""{
+			http.Error(w, "Bad Request: Empty Input", http.StatusBadRequest)
+			return 
+		}
+        data.Result = "you typed: " + text + " " + "with banner: " + banner
+    }
+
+    tmpl, err := template.ParseFiles("templates/index.html")
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    tmpl.Execute(w, data)
 }
 
 func main() {
