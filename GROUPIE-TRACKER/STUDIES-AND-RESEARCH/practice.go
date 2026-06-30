46679b832b02924fe0a515eb4135ceab06c4cf5e
pijohn <epictalesk@gmail.com>
2026-06-30 17:14:27 +0100

    added a new change

--- GROUPIE-TRACKER/STUDIES-AND-RESEARCH/practice.go
+++ GROUPIE-TRACKER/STUDIES-AND-RESEARCH/practice.go
@@ -16,6 +16,34 @@ type Artist struct {
 
 }
 
+type LocationIndex struct{ 
+	ID          int         `json:"id"`	
+	Locations   []string    `json:"locations"`
+	Dates       string      `json:"dates"`
+}
+
+type Locations struct {
+	Index []LocationIndex  `json:"index"`
+}
+
+type DateIndex struct{
+	ID     int     `json:"id"`
+	Dates  []string `json:"dates"`
+}
+
+type Dates struct {
+	Index []DateIndex `json:"index"`
+}
+
+type RelationIndex struct {
+	ID            int                  `json:"id"`
+	Dateslocation map[string][]string  `json:"datesLocations"`
+}
+
+type Relation struct {
+	Index  []RelationIndex `json:"index"`
+}
+
 func main() {
 
 }
