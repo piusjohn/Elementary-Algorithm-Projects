e1cee348923bfb32afdbcae095e7b3f44415e55e
pijohn <epictalesk@gmail.com>
2026-07-01 10:55:26 +0100

    added handlers and template

--- GROUPIE-TRACKER/STUDIES-AND-RESEARCH/practice.go
+++ GROUPIE-TRACKER/STUDIES-AND-RESEARCH/practice.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 )
+
+var tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 type Artist struct {
 	ID            int       `json:"id"`
@@ -44,11 +53,75 @@ type Relation struct {
 	Index  []RelationIndex `json:"index"`
 }
 
-func fetchArtists(url string) ([]Artist, error) {
-   
+func fetch[T any](url string) (T, error) {
+	var zero T
+    resp, err := http.Get(url)
+	if err != nil {
+		return zero, fmt.Errorf("error parsing the Artist json")
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return zero, fmt.Errorf("error streaming resp.body")
+	}
+
+	var data T
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+	return data, nil
+}
+var artists []Artist
+var locations Locations
+var dates  Dates
+var relation Relation
+func main() {
+	var err error
+	artists, err = fetch[[]Artist]("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	locations, err = fetch[Locations]("https://groupietrackers.herokuapp.com/api/locations")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dates, err = fetch[Dates]("https://groupietrackers.herokuapp.com/api/dates")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	relation, err = fetch[Relation]("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/home", handleHome)
+	http.HandleFunc("/artist/{id}", handleArtist)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	
 }
 
+func handleHome(w http.ResponseWriter, r *http.Request){
+	tpl.ExecuteTemplate(w, "index.html", artists)
+}
 
-func main() {
-
+func handleArtist(w http.ResponseWriter, r *http.Request){
+	v := r.PathValue("id")
+	id, err := strconv.Atoi(v)
+	if err != nil{
+		fmt.Println("id conversion failed")
+		return
+	}
+	for _, a := range artists{
+		if a.ID == id {
+			tpl.ExecuteTemplate(w, "artist.html", a)
+			return
+		}
+	}
+	http.Error(w, "message: Artist not found", http.StatusNotFound)
 }
